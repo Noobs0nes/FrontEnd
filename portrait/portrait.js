556528f638b03002f36c357bcb437342c3ff0a45
Noobs0nes <Noobs0nes@gmail.com>
2024-10-25 11:06:33 -0300

    Dynamic Life

---
+++ portrait/portrait.js
@@ -0,0 +1,13 @@
+const inputVida = {
+    "atual": document.getElementById("vidaAtual"),
+    "total": document.getElementById("vidaTotal"),
+    "barra": document.documentElement.style
+};
+
+inputVida.atual.onblur = () => {
+    let vidaAtual = inputVida.atual.textContent;
+    let vidaTotal = inputVida.total.textContent;
+    let value = vidaAtual/vidaTotal*100;
+    let percent = value +'%';
+    inputVida.barra.setProperty('--VIDA-ATUAL-PERCENT', percent)
+}
