cfe6e72e795c58977f3189591e5b3f93d21e622b
Noobs0nes <Noobs0nes@gmail.com>
2024-10-25 12:47:20 -0300

    dhdfhghd

--- portrait/portrait.js
+++ portrait/portrait.js
@@ -8,8 +8,14 @@ inputVida.atual.onchange = () => {
     let vidaAtual = inputVida.atual.value;
     let vidaTotal = inputVida.total.value;
     let value = (Number(vidaAtual)/Number(vidaTotal))*100;
-    let valueExtra = (value-100) +"%";
+    let valueExtra = (value-100);
+    let percentExtra;
+    if(valueExtra>0){
+        percentExtra = valueExtra+'%';
+    } else{
+        percentExtra = '0%'
+    }
     let percent = value +'%';
-    inputVida.barra.setProperty('--VIDA-EXTRA-PERCENT', valueExtra)
+    inputVida.barra.setProperty('--VIDA-EXTRA-PERCENT', percentExtra)
     inputVida.barra.setProperty('--VIDA-ATUAL-PERCENT', percent)
 }
