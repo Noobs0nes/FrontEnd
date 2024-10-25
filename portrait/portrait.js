6969daa44a478d361b9788de6e425c684c102ef3
Noobs0nes <Noobs0nes@gmail.com>
2024-10-25 11:17:29 -0300

    EHE.

--- portrait/portrait.js
+++ portrait/portrait.js
@@ -4,7 +4,7 @@ const inputVida = {
     "barra": document.documentElement.style
 };
 
-inputVida.atual.onblur = () => {
+inputVida.atual.oninput = () => {
     let vidaAtual = inputVida.atual.textContent;
     let vidaTotal = inputVida.total.textContent;
     let value = vidaAtual/vidaTotal*100;
